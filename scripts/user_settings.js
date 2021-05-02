d79979e81c7457435be0250f191f36c7d3976b89
Rakin Riza <rriza@uco.edu>
2021-05-02 07:32:46 +0600

    t

--- scripts/user_settings.js
+++ scripts/user_settings.js
@@ -1,38 +1,85 @@
-// 
+var password = document.getElementById("password")
+	, confirm_password = document.getElementById("confirmPassword");
+var email = document.getElementById("email")
+	, confirm_email = document.getElementById("confirmEmail");
 
-let user;
+document.getElementById('signupLogo').src = "design_files/images/UCO_logo.png";
+enableSubmitButton();
 
-function validateSignupForm(event) {
-	event.preventDefault();
-	const { target } = event;
-	const email = target.email.value;
-	const confirmEmail = target.confirmEmail.value;
-	const password = target.password.value;
-	const newPassword = target.newPassword.value;
-	const confirmPassword = target.confirmPassword.value;
+function validateEmail() {
+	if (password.value != confirm_email.value) {
+		confirm_email.setCustomValidity("Emails Don't Match");
+		return false;
+	} else {
+		confirm_email.setCustomValidity('');
+		return true;
+	}
+}
+password.onchange = validateEmail;
+confirm_password.onkeyup = validateEmail;
 
-	if (email !== confirmEmail) return alert("Confirm Email does not match!")
-	if (newPassword !== confirmPassword) return alert("Confirm Passowrd does not match!")
-
-	authDatabase.signInWithEmailAndPassword(user.email, password)
-		.then(({ user }) => user.updateEmail(email))
-		.then(_ => {
-			return Promise.all[
-				user.updatePassword(newPassword),
-				cloudDatabase.doc(`users/${user.uid}`).set({
-					Email: email,
-				}, { merge: true })
-			]
-		})
-		.then(_ => {
-			target.reset();
-			alert('Success')
-		})
-		.catch(err => alert(err.message))
+function validatePassword() {
+	if (password.value != confirm_password.value) {
+		confirm_password.setCustomValidity("Passwords Don't Match");
+		return false;
+	} else {
+		confirm_password.setCustomValidity('');
+		return true;
+	}
 }
 
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
 
-const unsubscribeFromAuth = firebase.auth().onAuthStateChanged(currentUser => {
-	if (!currentUser) return window.location.href = "/signin";
-	user = currentUser;
-})
+function enableSubmitButton() {
+	document.getElementById('submitButton').disabled = false;
+	document.getElementById('loader').style.display = 'none';
+}
+
+function disableSubmitButton() {
+	document.getElementById('submitButton').disabled = true;
+	document.getElementById('loader').style.display = 'none';
+}
+
+function validateSignupForm() {
+	var form = document.getElementById('signupForm');
+
+	for (var i = 0; i < form.elements.length; i++) {
+		if (form.elements[i].value === '' && form.elements[i].hasAttribute('required')) {
+			console.log('There are some required fields!');
+			return false;
+		}
+	}
+
+	if (!validatePassword()) {
+		return false;
+	}
+
+	if (!validateEmail()) {
+		return false;
+	}
+
+	onSignup();
+}
+
+function onSignup() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+
+		disableSubmitButton();
+
+		if (this.readyState == 4 && this.status == 200) {
+			enableSubmitButton();
+		}
+		else {
+			console.log('AJAX call failed!');
+			setTimeout(function () {
+				enableSubmitButton();
+			}, 1000);
+		}
+
+	};
+
+	xhttp.open("GET", "ajax_info.txt", true);
+	xhttp.send();
+}
